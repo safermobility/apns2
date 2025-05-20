849f56a51cd2bd1a0b8b1733998c216f7f096c74
Moshe Katz <kohenkatz@gmail.com>
2025-05-19 20:54:27 -0400

    temp include only cjs

--- dist/esm/index.js
+++
@@ -1,4 +0,0 @@
-export * from "./apns.js";
-export * from "./errors.js";
-export * from "./notifications/notification.js";
-export * from "./notifications/silent-notification.js";
--- dist/esm/notifications/constants/priority.js
+++
@@ -1,6 +0,0 @@
-export var Priority;
-(function (Priority) {
-    Priority[Priority["immediate"] = 10] = "immediate";
-    Priority[Priority["throttled"] = 5] = "throttled";
-    Priority[Priority["low"] = 1] = "low";
-})(Priority || (Priority = {}));
